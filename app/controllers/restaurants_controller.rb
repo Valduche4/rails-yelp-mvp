db094ab06ef62f3045d68db21f6cf950fca40042
Valentin de Laminne <valentindelaminne@hotmail.be>
2022-05-18 14:11:13 +0200

    Rails new with frontend and form gems

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,41 @@
+class RestaurantsController < ApplicationController
+
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restau_param)
+    @restaurant.save
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(restau_param)
+  end
+
+  def destroy
+    @restaurant.destroy
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restau_param
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
